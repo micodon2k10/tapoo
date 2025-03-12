d845747fdaf73966b9aefa02315f88fb6d5a098c
Michael Eze <ugwueze.michael@gmail.com>
2025-03-12 03:15:11 +0000

    (Chore): Add code climate CI Configuration

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 ### Tapoo
 
 [![CircleCI](https://circleci.com/gh/dmigwi/tapoo.svg?style=svg)](https://circleci.com/gh/dmigwi/tapoo)
+[![Maintainability](https://api.codeclimate.com/v1/badges/dcfb1148523ea3366f41/maintainability)](https://codeclimate.com/github/dmigwi/tapoo/maintainability)
+[![Test Coverage](https://api.codeclimate.com/v1/badges/dcfb1148523ea3366f41/test_coverage)](https://codeclimate.com/github/dmigwi/tapoo/test_coverage)
 
 Tapoo is a terminal maze runner, hide and seek game. One player hides in the maze and the other one finds them.
--- tapoo.go
+++ tapoo.go
@@ -1,17 +1,98 @@
 package main
 
 import (
-	"fmt"
-	"time"
+	"strconv"
+
+	termbox "github.com/nsf/termbox-go"
 )
 
-func main() {
-	var (
-		m = 3
-	)
+var count = 0
 
-	for i := 1; i < 20; i++ {
-		fmt.Printf("\t# Random No. %+v \n", time.Now().UnixNano()%int64(m))
+func drawMaze() {
+	const coldef = termbox.ColorDefault
+	termbox.Clear(coldef, coldef)
+	w, h := termbox.Size()
+
+	count++
+
+	if count%2 == 0 {
+		termbox.HideCursor()
+	} else {
+		termbox.SetCursor(30, 30)
 	}
 
+	midy := h / 3
+	midx := (w - 30) / 3
+
+	midy = 1
+	midx = 1
+
+	termbox.SetCell(midx-1, midy, '│', coldef, coldef)
+	termbox.SetCell(midx+30, midy, '│', coldef, coldef)
+	termbox.SetCell(midx-1, midy-1, '┌', coldef, coldef)
+	termbox.SetCell(midx-1, midy+1, '└', coldef, coldef)
+	termbox.SetCell(midx+30, midy-1, '┐', coldef, coldef)
+	termbox.SetCell(midx+30, midy+1, '┘', coldef, coldef)
+
+	for i := 0; i < 30; i++ {
+		termbox.SetCell(midx+i, midy-1, '-', coldef, coldef)
+	}
+
+	for i := 0; i < 30; i++ {
+		termbox.SetCell(midx+i, midy+1, '-', coldef, coldef)
+	}
+
+	for i, c := range "Press ESC to quit" {
+		termbox.SetCell(midx+6+i+count, midy+3, c, coldef, coldef)
+	}
+
+	for i, c := range strconv.Itoa(h) + "= height" {
+		termbox.SetCell(midx+6+i, midy+4, c, coldef, coldef)
+	}
+
+	for i, c := range strconv.Itoa(w) + "= width" {
+		termbox.SetCell(midx+6+i, midy+5, c, coldef, coldef)
+	}
+
+	termbox.Flush()
+}
+
+func main() {
+	err := termbox.Init()
+	if err != nil {
+		panic(err)
+	}
+
+	defer termbox.Close()
+	termbox.SetInputMode(termbox.InputEsc)
+
+	drawMaze()
+
+mainloop:
+	for {
+		switch ev := termbox.PollEvent(); ev.Type {
+		case termbox.EventKey:
+			switch ev.Key {
+			case termbox.KeyEsc:
+				break mainloop
+			case termbox.KeyArrowLeft, termbox.KeyCtrlB:
+			case termbox.KeyArrowRight, termbox.KeyCtrlF:
+			case termbox.KeyBackspace, termbox.KeyBackspace2:
+			case termbox.KeyDelete, termbox.KeyCtrlD:
+			case termbox.KeyTab:
+			case termbox.KeySpace:
+			case termbox.KeyCtrlK:
+			case termbox.KeyHome, termbox.KeyCtrlA:
+			case termbox.KeyEnd, termbox.KeyCtrlE:
+			default:
+				if ev.Ch != 0 {
+				}
+			}
+		case termbox.EventError:
+			panic(ev.Err)
+		}
+
+		drawMaze()
+
+	}
 }
